3f06a590e1d7d67105e609db0a87ebbccf296213
Yuri Gorokhov <yurig@apple.com>
2025-07-07 09:04:43 -0700

    Fixing scraper deployment

--- surfboard_scraper/deploy/install.sh
+++ surfboard_scraper/deploy/install.sh
@@ -2,8 +2,12 @@
 
 set -eux
 
-cp surfboard-scraper.service /lib/systemd/system/
+systemctl disable surfboard-scraper
 systemctl stop surfboard-scraper
+
+tar zxvf surfboard_scraper.tar.gz -C /opt
+cp /opt/surfboard_scraper/surfboard-scraper.service /etc/systemd/system/
+
 systemctl daemon-reload
 systemctl start surfboard-scraper
 systemctl enable surfboard-scraper
